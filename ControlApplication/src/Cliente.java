f3c00f44a63614e78fc44a7bbd67fd8c27bb76ba
André <Andr@Andre.home>
2017-12-21 23:47:30 +0000

    Encapsulamento, Restrutura do GUI, Incluido TABs, Tratamento de exceções, nomes adequeados a todas as variáveis.

--- ControlApplication/src/Cliente.java
+++ ControlApplication/src/Cliente.java
@@ -8,16 +8,18 @@ import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
-
+import java.net.SocketTimeoutException;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -31,34 +33,61 @@ import javax.swing.*;
 public class Cliente {
 
      //meter encapsulamento
-    public final byte SETDATE = 4;
-    public final byte SETTIME = 5;
-    public final byte SETCOLOR = 3;
-    public final byte SETMODE = 2;
-    public final byte  ACK = 0; //echo
-    //public final byte setMode = 3;
-    public final byte  requestTemp = 6;
-    public final byte  sendTemp = 7;
+    private final byte SETDATE = 4;
+    private final byte SETTIME = 5;
+    private final byte SETCOLOR = 3;
+    private final byte SETMODE = 2;
+    private final byte  ACK = 0; //echo
+    private final byte  requestTemp = 6;
+    private final byte  sendTemp = 7;
     private Socket cliente = null;
     private DataInputStream din;
     private DataOutputStream dout;
-    public TempThread temp = null; //meter private
+    private TempThread temp = null; //meter private
     private String Address;
     private int Port;
-    NewJFrame frame;
-    final int PORT = 10101;//6442
+    private Janela frame;
+    final int PORT = 6442;//6442
     private boolean is_Connected = false;
     
     
-    public Cliente(String Address, int Port, boolean tipoConnect, NewJFrame frame) throws IOException{ //adicionar porta
+    public Cliente(String Address, int Port, boolean tipoConnect, Janela frame) throws IOException { //adicionar porta
         if(tipoConnect){
             this.Address = Address;
         }
             this.Port = PORT;
             this.frame = frame;              
-            Initialize(tipoConnect);
-            
+            if(!Initialize(tipoConnect))
+            {
+                cliente = null;
+            }  
     }
+    public Socket getCliente() {
+        return cliente;
+    }
+    
+     public byte getSETCOLOR() {
+        return SETCOLOR;
+    }
+
+    public TempThread getTemp() {
+        return temp;
+    }
+        
+    /**
+     * @return the SETMODE
+     */
+    public byte getSETMODE() {
+        return SETMODE;
+    }
+
+    /**
+     * @return the requestTemp
+     */
+    public byte getRequestTemp() {
+        return requestTemp;
+    }
+    
         class TempThread implements Runnable { // Thread temp
             
         Thread t;
@@ -80,7 +109,6 @@ public class Cliente {
              this.dout = dout;
              temperaturas = new ArrayList<Float>();
              initializaFile("temperatura");
-             
         }
         
         private void initializaFile(String nome) throws IOException{
@@ -101,9 +129,7 @@ public class Cliente {
                 
             DateFormat dataFormat = new SimpleDateFormat ("yyyy/MM/dd HH:mm:ss");
             Date date = new Date();
-            
-            
-            System.out.println("Chegou aki");
+
             bw.write("\"Temperatura: \""+";"+"\""+temperaturas.get(tamanho - 1).floatValue()+" ºC"+"\"" + ";"+ "\""+dataFormat.format(date)+"\"");
             //+" ºC"+ " "+ dataFormat.format(date));"
             
@@ -127,7 +153,7 @@ public class Cliente {
                             String temperatura = new String();
                             
                             temperatura = String.format("%.2f ºC", temper);
-                            frame.jLabel5.setText(temperatura);
+                            frame.labelTemp.setText(temperatura);
                         }
                     }
                     ) ;
@@ -137,22 +163,20 @@ public class Cliente {
         @Override
         public void run(){
             
-            while(true){
+            while(!Thread.interrupted()){
                 try {
                     Thread.sleep(1000); // requests de 1 a 1 secs
                 } catch (InterruptedException ex) {
                     Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 try {
-                    sendData(requestTemp);
+                    sendData(getRequestTemp());
                     
                     din.read();
                     
                     tempe[0] = din.read();
                     tempe[1] = din.read();
                     tempe[2] = din.read();
-                    
-                    
                     
                     float temper = tempe[1] + ((float) tempe[2]/100);
                     if(tempe[0] == 1)
@@ -162,7 +186,7 @@ public class Cliente {
                     System.out.printf("Temperatura: %.2f ºC \n",temper);
                     temperaturas.add(new Float(temper));
                     escreveFich();
-                    setTempLabel(temper);
+                    //setTempLabel(temper);
                     
                     
                 } catch (IOException ex) {
@@ -187,23 +211,37 @@ public class Cliente {
         }     
     }
                       
-    public void Initialize(boolean tipoConnect) throws IOException{ // para os connects e disconnects sucessivos (se houver)
+    public boolean Initialize(boolean tipoConnect) throws IOException { // para os connects e disconnects sucessivos (se houver)
                  
             if(!tipoConnect){  
                      
                 if(!findAddr()){
                     System.out.println("Nao encontrado");
-                    return;
+                    return false;
                 }
                 
             }
             else {
-               
+               try{
+                   
                 cliente = new Socket();
                 cliente.connect(new InetSocketAddress(Address, Port), 100); // sobrepoem o endereço do construtor pelo address do findAddr
+                
+               }
+               catch (SocketTimeoutException e) {
+                   System.out.println("Erro de Timeout");
+                   
+                    return false;
+	        } 
+               catch (IOException e) {
+                  System.out.println("Input");
+	         return false;
+                }
+               
             }    
        
         System.out.println("Utilizador ligado com sucesso no endereço "+cliente.getInetAddress());
+        
         is_Connected = true;
         try {
             dout =  new DataOutputStream(cliente.getOutputStream());
@@ -213,6 +251,7 @@ public class Cliente {
         }
         temp = new TempThread(din,dout); //inicia a thread
         
+        return true;
     }
     
     public boolean getisConnected(){
@@ -224,6 +263,10 @@ public class Cliente {
     {
         dout = null;
         din = null;
+        temp.din = null;
+        temp.dout = null;
+        temp.t.interrupt();
+        
         try {
             cliente.close();
         } catch (IOException ex) {
@@ -287,14 +330,9 @@ public class Cliente {
         dout.write(tipo);
        
  }
-    
-    public void receiveData(){
-        System.out.println("Teste");
-    }
-    
+
     public boolean findAddr() throws UnknownHostException, IOException{ // ALPHA MUITO ALPHA
         InetAddress localhost = InetAddress.getLocalHost();
-        //int flag = 0; // indica que conseguiu se conectar - 0 = falhou e 1 = ligou
         
         byte[] ip = localhost.getAddress();
         
@@ -305,7 +343,7 @@ public class Cliente {
             InetAddress address = InetAddress.getByAddress(ip);
             if (address.isReachable(100))
             {
-                System.out.println("ENCONTROU O ENDEREÇO: "+address.getHostAddress());
+                System.out.println("Encontrou o endereço na rede: "+address.getHostAddress());
                 Address = address.getHostAddress();
                 try
                 {
@@ -315,11 +353,9 @@ public class Cliente {
                 return true;
                 } 
                 catch (IOException ex) {
-                    System.out.println("Keep Looking");
+                    System.out.println("Procurando...");
                     cliente.close();
-                  }
-                    
-            System.out.println("Hello");
+                  }              
             }
         }
             return false;
